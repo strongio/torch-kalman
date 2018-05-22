45ccb0fc2fb2e615d0e52120f1aa367d27cce6e1
Jacob <jwdink1@gmail.com>
2018-05-22 11:34:46 -0500

    Some more state -> state_element renaming

--- torch_kalman/design/design_matrix/initial_state.py
+++ torch_kalman/design/design_matrix/initial_state.py
@@ -3,21 +3,28 @@ from torch.autograd import Variable
 
 from torch_kalman.design.design_matrix import DesignMatrix
 from torch_kalman.design.nn_output import NNOutput
+from torch_kalman.utils.torch_utils import expand
 
 
 class InitialState(DesignMatrix):
-    def __init__(self, states):
+    def __init__(self, state_elements):
         """
         Not really a design-matrix, but uses almost all the same methods.
 
-        :param states: The States.
+        :param state_elements: The StatesElements.
         """
-        self.states = states
+        self.state_elements = state_elements
         self.initial_state_means = {}
         self.initial_state_std_devs = {}
         super().__init__()
         self._means_template = None
         self._cov_template = None
+        self._template = {'means': self.means_template, 'cov': self.cov_template}
+
+    def reset(self):
+        self._means_template = None
+        self._cov_template = None
+        self.batch_cache = {}
 
     @property
     def template(self):
@@ -26,8 +33,8 @@ class InitialState(DesignMatrix):
     @property
     def means_template(self):
         if self._means_template is None:
-            num_states = len(self.states)
-            self._means_template = Variable(torch.zeros(num_states, 1))
+            num_state_elements = len(self.state_elements)
+            self._means_template = Variable(torch.zeros(num_state_elements, 1))
             for i, initial_mean in self.initial_state_means.items():
                 self._means_template[i] = initial_mean()
         return self._means_template
@@ -35,8 +42,8 @@ class InitialState(DesignMatrix):
     @property
     def cov_template(self):
         if self._cov_template is None:
-            num_states = len(self.states)
-            self._cov_template = Variable(torch.zeros(num_states, num_states))
+            num_state_elements = len(self.state_elements)
+            self._cov_template = Variable(torch.zeros(num_state_elements, num_state_elements))
             for i, initial_std_dev in self.initial_state_std_devs.items():
                 self._cov_template[i, i] = torch.pow(initial_std_dev(), 2)
         return self._cov_template
@@ -44,17 +51,17 @@ class InitialState(DesignMatrix):
     def register_variables(self):
         self.nn_outputs = []
 
-        for i, (state_id, state) in enumerate(self.states.items()):
-            # state mean:
-            self.initial_state_means.update({i: state.initial_mean})
-            if isinstance(state.initial_mean, NNOutput):
-                state.initial_mean.add_design_mat_idx((i, 0))
-                self.nn_outputs.append(state.initial_mean)
+        for i, (_, state_element) in enumerate(self.state_elements.items()):
+            # state_element mean:
+            self.initial_state_means.update({i: state_element.initial_mean})
+            if isinstance(state_element.initial_mean, NNOutput):
+                state_element.initial_mean.add_design_mat_idx((i, 0))
+                self.nn_outputs.append(state_element.initial_mean)
 
-            # state std-dev:
-            self.initial_state_std_devs.update({i: state.initial_std_dev})
-            if isinstance(state.initial_std_dev, NNOutput):
-                raise NotImplementedError("Found a NNOutput initial state std-dev, which is not currently supported.")
+            # state_element std-dev:
+            self.initial_state_std_devs.update({i: state_element.initial_std_dev})
+            if isinstance(state_element.initial_std_dev, NNOutput):
+                raise NotImplementedError("Found a NNOutput for a state's std-dev, which is not currently supported.")
 
     def create_means_for_batch(self, time, **kwargs):
         if time != 0:
@@ -65,5 +72,7 @@ class InitialState(DesignMatrix):
     def create_cov_for_batch(self, time, **kwargs):
         if time != 0:
             raise Exception("InitialState is for time-zero only.")
+        bs = kwargs['kf_input'].data.shape[0]
 
-        return super()._create_for_batch(time=time, template=self.cov_template, **kwargs)
+        # no NNOutput supported, so just expand
+        return expand(self.cov_template, bs)
