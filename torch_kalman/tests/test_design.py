599169d321d8a78de62552b4f16b9cd9a1426b5a
Jacob <jacobwdink@gmail.com>
2018-09-14 10:24:31 -0500

    Test design.set_transition

--- torch_kalman/tests/test_design.py
+++ torch_kalman/tests/test_design.py
@@ -1,5 +1,10 @@
+from copy import deepcopy
+from unittest import mock
+
+import torch
 from numpy import array_equal
 from torch import Tensor
+from torch.nn import Parameter
 
 from torch_kalman.design import Design
 from torch_kalman.measure import Measure
@@ -35,7 +40,7 @@ class TestDesign(TestCaseTK):
     def test_design_f(self):
         # design
         design = self.make_usable_design()
-        batch_design = design.for_batch(1)
+        batch_design = design.for_batch(2)
 
         # F doesn't require grad:
         self.assertFalse(batch_design.F().requires_grad)
@@ -44,6 +49,34 @@ class TestDesign(TestCaseTK):
         design_F = batch_design.F()[0].data.numpy()
         manual_F = block_diag(*[process.F()[0].data.numpy() for process in batch_design.processes.values()])
         self.assertTrue(array_equal(design_F, manual_F))
+
+        # we can't add batch-specific values that have already been set:
+        vd_init = torch.randn(2)
+        vel_damp = Parameter(vd_init)
+        with self.assertRaises(ValueError) as cm:
+            batch_design.processes['0'].set_transition(from_element='velocity', to_element='velocity', values=vel_damp)
+        self.assertEqual(first=cm.exception.args[0],
+                         second="The transition from 'velocity' to 'velocity' was already set for this Process, so can't "
+                                "give it batch-specific values.")
+
+        # but when we set batch-specific values that can be set, it works:
+        # sort of like a mock; we remove the velocity -> velocity transition so we can set it.
+        batch_design.processes['0'].process.transitions = deepcopy(batch_design.processes['0'].process.transitions)
+        batch_design.processes['0'].process.transitions.pop('velocity')
+
+        # now this will work:
+        batch_design.processes['0'].set_transition(from_element='velocity', to_element='velocity', values=vel_damp)
+
+        # and the correct value should show up in the design-matrix:
+        self.assertAlmostEqual(batch_design.F()[0, 1, 1].item(), vd_init[0].item())
+        self.assertAlmostEqual(batch_design.F()[1, 1, 1].item(), vd_init[1].item())
+
+        # because the batch-specific value was a Parameter, now the batch.F requires_grad
+        self.assertTrue(batch_design.F().requires_grad)
+
+        # must match batch-size
+        with self.assertRaises(AssertionError):
+            batch_design.processes['0'].set_transition(from_element='velocity', to_element='velocity', values=torch.randn(3))
 
     def test_design_q(self):
         # design
