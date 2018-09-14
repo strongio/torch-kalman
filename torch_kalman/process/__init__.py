89895535c28f74755b76b95ac5eeb52392260f76
Jacob <jacobwdink@gmail.com>
2018-09-14 10:23:56 -0500

    Add docstring to Process
    Clarifies usage of None values

--- torch_kalman/process/__init__.py
+++ torch_kalman/process/__init__.py
@@ -12,6 +12,18 @@ class Process:
                  id: str,
                  state_elements: Sequence[str],
                  transitions: Dict[str, Dict[str, float]]):
+        """
+
+        :param id: A unique string identifying the process.
+        :param state_elements: A sequence of names for the states that make up this process.
+        :param transitions: A dictionary whose structure is easier to understand by example. For example:
+            `transitions=dict(position = dict(position = 1.0, velocity = 1.0))`
+        is designed to be read like an equation:
+            position_t2 = 1.0 * position_t1 + 1.0 * velocity_t1
+        That is, we specify a Dict whose keys are states being transitioned *to*, and whose values are themselves Dicts
+        specifying the states *from* which we are transitioning. When specifying None for a value, this means that the value
+        needs to be generated on a per-batch basis, via ProcessForBatch.set_transition.
+        """
 
         self.id = str(id)
         self._state_element_idx = None
