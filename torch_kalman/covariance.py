bf3606e4e90ad5696d155bd962b58210472035e4
Jacob <jacobwdink@gmail.com>
2018-09-20 20:46:28 -0500

    Remove unused classmethod from Covariance

--- torch_kalman/covariance.py
+++ torch_kalman/covariance.py
@@ -14,12 +14,3 @@ class Covariance(Tensor):
         L[np.tril_indices(n=n, k=-1)] = off_diag
         L[np.triu_indices(n=n, k=1)] = 0.
         return L.mm(L.t())
-
-    @classmethod
-    def from_diag(cls, diag: Parameter) -> Tensor:
-        n = len(diag)
-        out = Covariance(size=(n, n))
-        out[np.diag_indices(n)] = diag
-        out[np.tril_indices(n=n)] = 0.
-        out[np.triu_indices(n=n)] = 0.
-        return out
