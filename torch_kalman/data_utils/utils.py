2dcae5e616e1e25d7457689e8b88a31d68db45dc
Jacob <jacobwdink@gmail.com>
2018-10-19 12:26:10 -0500

    Move fourier_series function to proper file

--- torch_kalman/data_utils/utils.py
+++ torch_kalman/data_utils/utils.py
@@ -32,16 +32,3 @@ def tens_to_long(tens: Union[ndarray, Tensor], **kwargs) -> Sequence[Dict[str, U
             rows.append(new_row)
 
     return rows
-
-
-def fourier_series(time: Tensor, seasonal_period: int, parameters: Tensor):
-    dim1, dim2 = parameters.shape
-    assert dim2 == 2, f"Expected K X 2 matrix, got {(dim1, dim2)}."
-
-    out = torch.zeros_like(time)
-    for idx in range(dim1):
-        k = idx + 1
-        out += (parameters[idx, 0] * torch.sin(2. * pi * k * time / seasonal_period))
-        out += (parameters[idx, 1] * torch.cos(2. * pi * k * time / seasonal_period))
-
-    return out
---
+++ torch_kalman/utils.py
@@ -0,0 +1,15 @@
+import torch
+from torch import Tensor
+
+
+def fourier_series(time: Tensor, seasonal_period: int, parameters: Tensor):
+    dim1, dim2 = parameters.shape
+    assert dim2 == 2, f"Expected K X 2 matrix, got {(dim1, dim2)}."
+
+    out = torch.zeros_like(time)
+    for idx in range(dim1):
+        k = idx + 1
+        out += (parameters[idx, 0] * torch.sin(2. * pi * k * time / seasonal_period))
+        out += (parameters[idx, 1] * torch.cos(2. * pi * k * time / seasonal_period))
+
+    return out
