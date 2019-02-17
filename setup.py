b1c951d63bba0a1e66ffc5192a864f7f8b14540e
Jacob <jacobwdink@gmail.com>
2019-02-17 13:59:39 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='torch_kalman',
       version='0.1',
@@ -7,7 +7,7 @@ setup(name='torch_kalman',
       author='Jacob Dink',
       author_email='jacob.dink@strong.io',
       license='MIT',
-      packages=['torch_kalman'],
+      packages=[p for p in find_packages() if 'torch_kalman' in p],
       zip_safe=False,
       install_requires=['torch>=1.0.0', 'numpy>=1.14.2', 'tqdm'],
       test_suite='nose.collector',
