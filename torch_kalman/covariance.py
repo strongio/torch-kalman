cc97239b0337790c7cabe544b8ae669356071ed1
Jacob <jacobwdink@gmail.com>
2019-04-22 16:48:47 -0500

    Support batch Covariance.from_log_cholesky

--- torch_kalman/covariance.py
+++ torch_kalman/covariance.py
@@ -1,47 +1,40 @@
 import torch
-from torch import Tensor
 import numpy as np
-from torch.nn import Parameter
 
 from typing import Tuple, Optional
 
+from torch_kalman.utils import matrix_diag
 
-class Covariance(Tensor):
-    def __new__(cls, *args, **kwargs):
+
+class Covariance(torch.Tensor):
+    def __new__(cls, *args, **kwargs) -> 'Covariance':
         return super().__new__(cls, *args, **kwargs)
 
     @classmethod
     def from_log_cholesky(cls,
-                          log_diag: Parameter,
-                          off_diag: Parameter,
+                          log_diag: torch.Tensor,
+                          off_diag: torch.Tensor,
                           device: Optional[torch.device] = None) -> 'Covariance':
-        n = len(log_diag)
-        L = torch.empty(size=(n, n), device=device)
-        L[np.diag_indices(n)] = torch.exp(log_diag)
-        L[np.tril_indices(n=n, k=-1)] = off_diag
-        L[np.triu_indices(n=n, k=1)] = 0.
+        assert log_diag.shape[:-1] == off_diag.shape[:-1]
+        batch_dim = log_diag.shape[:-1]
 
-        out = Covariance(size=(n, n)).to(device)
-        out[:] = L.mm(L.t())
+        n = log_diag.shape[-1]
+        L = matrix_diag(torch.exp(log_diag))
+
+        idx = 0
+        for i in range(n):
+            for j in range(i):
+                L[..., i, j] = off_diag[..., idx]
+                idx += 1
+
+        out = Covariance(size=batch_dim + (n, n)).to(device)
+        perm_shape = tuple(range(len(batch_dim))) + (-1, -2)
+        out[:] = L.matmul(L.permute(perm_shape))
         return out
 
-    def to_log_cholesky(self) -> Tuple[Tensor, Tensor]:
+    def to_log_cholesky(self) -> Tuple[torch.Tensor, torch.Tensor]:
         n = self.shape[-1]
         L = torch.cholesky(self)
         off_diag = L[np.tril_indices(n=n, k=-1)]
         log_diag = torch.log(torch.diag(L))
         return log_diag, off_diag
-
-    @classmethod
-    def from_std_and_corr(cls,
-                          log_std_devs: Tensor,
-                          corr_arctanh: Tensor,
-                          device: Optional[torch.device] = None) -> 'Covariance':
-        if len(log_std_devs) != 2:
-            raise ValueError("This method can only be used for 2x2 covariance mats.")
-
-        std_diag = torch.diag(torch.exp(log_std_devs)).to(device=device)
-        corr_mat = torch.eye(2, device=device)
-        corr_mat[0, 1] = torch.tanh(corr_arctanh)
-        corr_mat[1, 0] = torch.tanh(corr_arctanh)
-        return torch.mm(torch.mm(std_diag, corr_mat), std_diag)
---
+++ torch_kalman/tests/test_covariance.py
@@ -0,0 +1,16 @@
+import torch
+from torch_kalman.covariance import Covariance
+from torch_kalman.tests import TestCaseTK
+
+
+class TestCovariance(TestCaseTK):
+    def test_from_log_cholesky(self):
+        covs = Covariance.from_log_cholesky(log_diag=torch.arange(1., 3.1).expand(3, -1),
+                                            off_diag=torch.arange(1., 3.1).expand(3, -1))
+
+        gt = torch.tensor([[7.3891, 2.7183, 5.4366],
+                           [2.7183, 55.5982, 24.1672],
+                           [5.4366, 24.1672, 416.4288]])
+        for cov in covs:
+            diff = (gt - cov).abs()
+            self.assertTrue((diff < .0001).all())
