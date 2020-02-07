dbc40fd7708043044cd0dfe418607e1bbe2b7069
Jacob <jacobwdink@gmail.com>
2020-02-07 12:02:43 -0600

    Update air_quality_example.py

--- examples/air_quality_example.py
+++ examples/air_quality_example.py
@@ -20,7 +20,7 @@ import torch
 from torch.optim import LBFGS
 
 from torch_kalman.kalman_filter import KalmanFilter
-from torch_kalman.process import LocalLevel, Season, FourierSeason, LinearModel
+from torch_kalman.process import LocalLevel, LocalTrend, Season, FourierSeason, LinearModel
 from torch_kalman.utils.data import TimeSeriesDataset
 from torch_kalman.utils.simulate import simulate_daily_series
 
@@ -33,9 +33,11 @@ np.random.seed(2020-1-3)
 torch.manual_seed(2020-1-3)
 # -
 
-# ## UCI Machine Learning Data Repository - Beijing Multi-Site Air-Quality Data Set
+# # Introduction to Torch-Kalman
+
+# ## Beijing Multi-Site Air-Quality Dataset
 #
-# This dataset includes hourly air pollutants data from 12 nationally-controlled air-quality monitoring sites. The air-quality data are from the Beijing Municipal Environmental Monitoring Center. 
+# This dataset comes from the [UCI Machine Learning Data Repository](https://archive.ics.uci.edu/ml/datasets/Beijing+Multi-Site+Air-Quality+Data). It includes hourly air pollutants data from 12 nationally-controlled air-quality monitoring sites. The air-quality data are from the Beijing Municipal Environmental Monitoring Center. 
 #
 # The meteorological data in each air-quality site are matched with the nearest weather station from the China Meteorological Administration. The time period is from March 1st, 2013 to February 28th, 2017. 
 #
@@ -71,138 +73,96 @@ except FileNotFoundError:
     df_aq.to_csv("./PRSA2017_Data_20130301-20170228.csv", index=False)
 
 del df_aq['No']
-df_aq['time'] = pd.to_datetime(df_aq.loc[:,['year','month','day','hour']])    
+df_aq['time'] = pd.to_datetime(df_aq.loc[:,['year','month','day','hour']])
+df_aq = df_aq.rename(columns={'PM2.5':'PM2p5'})
 # -
 
-# For now, we'll focus on generating daily forecasts. We'll downsample by taking the values from the end of the workday.
+SPLIT_DT = np.datetime64('2016-02-22')
 
-df_aq_daily = df_aq.\
-    loc[df_aq['hour'] == 17,:].\
-    drop(columns=['hour']).\
-    reset_index(drop=True).\
-    assign(date = lambda df: df.pop('time').astype('datetime64[D]'))
-df_aq_daily
+# To simplify the example, we'll focus on weekly averages for two measures: PM10 and SO2. Since these measures are strictly positive, we log-transform them.
 
-# We'll focus on predicting CO levels. 
+# +
+df_aq_weekly = df_aq.\
+    assign(date = lambda df: df['time'].astype('datetime64[D]') - pd.to_timedelta(df['time'].dt.dayofweek, unit='d')).\
+    drop(columns = ['year','month','day','hour']).\
+    groupby(['date','station']).\
+    agg('mean').\
+    reset_index()
 
-df_aq_daily.query("station=='Aotizhongxin'").plot('date','CO', figsize=(15,3))
-df_aq_daily.query("station=='Aotizhongxin'").plot('date','CO', figsize=(15,3),logy=True)
+col_means = df_aq_weekly.loc[df_aq_weekly['date'] < SPLIT_DT,:].mean()
+col_stds = df_aq_weekly.loc[df_aq_weekly['date'] < SPLIT_DT,:].std()
 
-# This predictor is strictly positive and its variance is variable throughout the year, so we'll use a log-transform. We'll also scale the data so that it's centered around zero on the log scale.
+measures = ['SO2','PM10']
+measures_pp = [m + '_log10_scaled' for m in measures]
 
-df_aq_daily['CO_log_scaled'] = np.log10(df_aq_daily['CO'] / df_aq_daily['CO'].mean())
+df_aq_weekly[measures_pp] = np.log10(df_aq_weekly[measures] / col_means[measures])
 
-# ## Single Station
-#
-# As a first example, let's generate predictions for a single station.
-#
-# We'll first create a `TimeSeriesDataset` object. This behaves similarly to pytorch's `TensorDataset`, but contains useful metadata about the tensors relating to:
-#
-# - The groups (stations) in the dataset, indexed in the tensor's first dimension.
-# - The times in the dataset, indexed in the tensor's second dimension.
-# - The measures in the dataset, indexed in the tensor's third dimension.
-#
-# For now we have one group (Aotizhongxin) and one measure (log-transformed CO-levels).
 
-dataset_aoti = TimeSeriesDataset.from_dataframe(
-    dataframe=df_aq_daily.query("station=='Aotizhongxin'"), 
-    dt_unit='D',
-    group_colname='station',
-    time_colname='date',
-    measure_colnames=['CO_log_scaled']
+df_aq_weekly
+# -
+
+# The `TimeSeriesDataset` is similar to PyTorch's native `TensorDataset`, but includes metadata about the time-serieses that are in the dataset, such as the name for each (here, the station-name), the timestamps for each datapoint, and the measure-names.
+
+# +
+dataset_all = TimeSeriesDataset.from_dataframe(
+    dataframe=df_aq_weekly,
+    dt_unit='W',
+    measure_colnames=measures_pp,
+    group_colname='station', 
+    time_colname='date'
 )
 
-SPLIT_DT = np.datetime64('2016-03-01')
-dataset_aoti_train, dataset_aoti_val = dataset_aoti.train_val_split()
-dataset_aoti_train, dataset_aoti_val
+dataset_train, dataset_val = dataset_all.train_val_split(dt=SPLIT_DT)
+dataset_train, dataset_val
+# -
 
-# In order to create our Kalman Filter, we need *measures* and *processes*. 
+# In `torch-kalman` we think of a model in terms of its *processes*. Processes are how we describe what makes the system transition from one state to another, and how this latent system can be translated into the observable *measures*. 
 #
-# Measures are simply our outcome(s) of interest. 
+# In time series applications, we often already have an intuitive understanding of such processes. There could be a trend, different seasonalities, exogneous factors, etc. `torch-kalman` comes with several flexible processes. Here, we will specify three processes:
 #
-# Processes are models we use to understand our system -- i.e. what makes the system transition from one state to another, and how this latent system can be translated into the observable *measures*. 
+# - The `LocalTrend` process captures long-range trends.
+# - The `LocalLevel` process captures short-range changes that tend to decay back to zero.
+# - The `FourierSeason` captures yearly seasonalities, using a [fourier-series](https://en.wikipedia.org/wiki/Fourier_series)
 #
-# In time series applications, we often already have an intuitive understanding of such processes. There could be a trend, different seasonalities, exogneous factors, etc. `torch-kalman` comes with several flexible processes. Here, we will define a `LocalLevel` (random walk) process as well as `FourierSeason` process that captures seasonality using a fourier-series.
+# Additionally, below shows off a few useful features of `torch-kalman`:
+#
+# - We are training on a multivarite time-series: that is, our time-series has two measures (SO2 and PM10) and our model will capture correlations across these.
+# - We are going to train on, and predictor for, multiple time-serieses (i.e. multiple stations) at once. 
+# - We are allowing the measure-variance -- the noise in the measures that cannot be captured by our processes -- to vary with the seasons, by passing 'seasonal' alias to `measure_var_predict`. (The `measure_var_predict` argument takes any `torch.nn.Module` that can be used for prediction, but 'seasonal' is an alias that tells the `KalmanFilter` to use a seasonal NN.)
 
-kf_aoti = KalmanFilter(
-    measures=['CO_log_scaled'],
-    processes=[
-        LocalLevel(id='local_level').add_measure('CO_log_scaled'),
-        FourierSeason(id='day_in_year', seasonal_period=365.25, dt_unit='D', K=2, fixed=True).add_measure('CO_log_scaled')
-    ]
-)
+# ## Our First Kalman Filter
+
+processes = []
+for measure in measures_pp:
+    processes.extend([
+        LocalTrend(
+            id=f'{measure}_trend', multi=.01
+        ).add_measure(measure),
+        LocalLevel(
+            id=f'{measure}_local_level',
+            decay=(.90,1.00)
+        ).add_measure(measure),
+        FourierSeason(
+            id=f'{measure}_day_in_year', seasonal_period=365.25 / 7., dt_unit='W', K=2, fixed=True
+        ).add_measure(measure)
+    ])
+kf_first = KalmanFilter(measures=measures_pp, 
+                      processes=processes, 
+                      measure_var_predict=('seasonal',dict(K=2,period='yearly',dt_unit='W')))
 
 # Since our dataset is fairly small, we'll use full-batch LBFGS.
 #
 # The output of the forward pass is a `StateBeliefOverTime` object. These represent the predictions (by default one-step-ahead) generated by the kalman-filter. These predictions come with a mean and covariance, and so can be evaluated against the actual data using a (negative) log-probability critierion:
 
 # +
-kf_aoti.opt = LBFGS(kf_aoti.parameters(), lr=.25, max_eval=10)
+kf_first.opt = LBFGS(kf_first.parameters(), lr=.25, max_eval=10)
 
 def closure():
-    kf_aoti.opt.zero_grad()
-    pred = kf_aoti(dataset_aoti_train.tensors[0], start_datetimes=dataset_aoti_train.start_datetimes)
-    loss = -pred.log_prob(dataset_aoti_train.tensors[0]).mean()
-    loss.backward()
-    return loss
-
-
-# -
-
-for epoch in range(10):
-    train_loss = kf_aoti.opt.step(closure).item()
-    with torch.no_grad():
-        pred = kf_aoti(dataset_aoti_val.tensors[0], start_datetimes=dataset_aoti_val.start_datetimes)
-        val_loss = -pred.log_prob(dataset_aoti_val.tensors[0]).mean().item()
-    print(f"EPOCH {epoch}, TRAIN LOSS {train_loss}, VAL LOSS {val_loss}")
-
-# The `StateBeliefOverTime` also has `to_dataframe()` and `plot()` methods, which are useful for visualizing our model's forecasts.
-
-pred = kf_aoti(
-    dataset_aoti_train.tensors[0], 
-    start_datetimes=dataset_aoti_train.start_datetimes,
-    out_timesteps=dataset_aoti.tensors[0].shape[1]
-)
-pred.plot(pred.to_dataframe(dataset_aoti), split_dt=SPLIT_DT)
-
-# We can also get a dataframe (and plot) of the components of the model, which are the measureable parts of the processes. 
-
-pred.plot(pred.to_dataframe(dataset_aoti, type='components'), split_dt=SPLIT_DT)
-
-# TODO: MAPE
-
-# ## Multi-Station
-#
-# `torch-kalman` facilitates training a model on multiple time-series. This lets the model share hard-to-learn parameters (like process and measure covariances) across groups, while tailoring predictions to each specific group (e.g. each group might have a different seasonal pattern). 
-#
-# For simplicity the below still uses full-batch training. If you need to split your data into batches see `TimeSeriesDataLoader`.
-
-# +
-dataset_all = TimeSeriesDataset.from_dataframe(
-    dataframe=df_aq_daily,
-    dt_unit='D',
-    group_colname='station',
-    time_colname='date',
-    measure_colnames=['CO_log_scaled']
-)
-
-dataset_train, dataset_val = dataset_all.train_val_split()
-dataset_train, dataset_val
-
-# +
-kf_all = KalmanFilter(
-    measures=['CO_log_scaled'],
-    processes=[
-        LocalLevel(id='local_level').add_measure('CO_log_scaled'),
-        FourierSeason(id='day_in_year', seasonal_period=365.25, dt_unit='D', K=2, fixed=True).add_measure('CO_log_scaled')
-    ]
-)
-
-kf_all.opt = LBFGS(kf_all.parameters(), lr=.25, max_eval=10)
-
-def closure():
-    kf_all.opt.zero_grad()
-    pred = kf_all(dataset_train.tensors[0], start_datetimes=dataset_train.start_datetimes)
+    kf_first.opt.zero_grad()
+    pred = kf_first(
+        dataset_train.tensors[0], 
+        start_datetimes=dataset_train.start_datetimes, 
+    )
     loss = -pred.log_prob(dataset_train.tensors[0]).mean()
     loss.backward()
     return loss
@@ -210,41 +170,64 @@ def closure():
 
 # -
 
-for epoch in range(10):
-    train_loss = kf_all.opt.step(closure).item()
+for epoch in range(15):
+    train_loss = kf_first.opt.step(closure).item()
     with torch.no_grad():
-        pred = kf_all(dataset_val.tensors[0], start_datetimes=dataset_val.start_datetimes)
+        pred = kf_first(
+            dataset_val.tensors[0], 
+            start_datetimes=dataset_val.start_datetimes
+        )
         val_loss = -pred.log_prob(dataset_val.tensors[0]).mean().item()
     print(f"EPOCH {epoch}, TRAIN LOSS {train_loss}, VAL LOSS {val_loss}")
 
-pred = kf_all(
+
+# The `StateBeliefOverTime` also has `to_dataframe()` and `plot()` methods, which are useful for visualizing our model's forecasts.
+
+# +
+def inverse_transform(df: pd.DataFrame, col_means: pd.Series) -> pd.DataFrame:
+    # inverse log10 transform:
+    df = df.\
+        assign(
+            measure=lambda df: df['measure'].str.replace('_log10_scaled',''),
+            _std=lambda df: (df['upper'] - df['lower']) / 1.96,
+            mean=lambda df: 10 ** ( df['mean'] + .5 * df.pop('_std') **2 ),
+            lower=lambda df: 10 ** ( df['lower']  ),
+            upper=lambda df: 10 ** ( df['upper']  ),
+            actual=lambda df: 10 ** ( df['actual'] ),
+        )
+    
+    # inverse scaling:
+    for col in ['mean','lower','upper','actual']:
+        df[col] *= df['measure'].map(col_means.to_dict())
+
+    return df
+
+pred = kf_first(
     dataset_train.tensors[0], 
     start_datetimes=dataset_train.start_datetimes,
     out_timesteps=dataset_all.tensors[0].shape[1]
 )
 
-# TODO: MAPE
+df_pred_aoti = inverse_transform(pred.to_dataframe(dataset_all), col_means)
 
-# ## With Exogenous Predictors
+pred.plot(df_pred_aoti.query("group=='Changping'"), split_dt=SPLIT_DT)
+# -
+
+# We can also get a dataframe (and plot) of the components of the model, which are the measureable parts of the processes. 
+
+pred.plot(pred.to_dataframe(dataset_all, type='components').query("group=='Changping'"), split_dt=SPLIT_DT)
+
+# ## Including Predictors
 #
 # In some contexts, we want to use exogenous predictors. For example, we may only be interested in one-step-ahead forecasts, and we can use 1-day-lagged predictors. Or we may be interested in [estimating counterfactuals](https://google.github.io/CausalImpact/CausalImpact.html).
-
-predictors_raw = ['TEMP', 'PRES', 'DEWP', 'WSPM', 'RAIN']
-for col in predictors_raw:
-    df_aq_daily.query("station=='Aotizhongxin'").plot('date', col, figsize=(15,3))
-
-# First, we'll center and scale our exogenous predictors, which is helpful for speeding up optimization:
+#
+# Here, we'll use the weather to predictor our measures of interest.
 
 # +
-df_predictors_daily = df_aq_daily.\
-    loc[:,['station', 'date', 'CO_log_scaled'] + predictors_raw].\
-    assign(RAIN_log1p = lambda df: np.log1p(df['RAIN']),
-           WSPM_log1p = lambda df: np.log1p(df['WSPM']))
+predictors = ['TEMP', 'PRES', 'DEWP']
+predictors_pp = [x + '_scaled' for x in predictors]
 
-predictors = ['TEMP', 'PRES', 'DEWP', 'WSPM_log1p', 'RAIN_log1p']
-
-pred_scaler = StandardScaler().fit(df_predictors_daily.loc[df_predictors_daily['date'] < SPLIT_DT, predictors])
-df_predictors_daily[predictors] = pred_scaler.transform(df_predictors_daily[predictors])
+df_aq_weekly[predictors_pp] = (df_aq_weekly[predictors] - col_means[predictors]) / col_stds[predictors]
 # -
 
 # Since a tensor of exogenous predictors is such a common use-case, `TimeSeriesDataset` supports passing `y_colnames` and `X_colnames` separately, instead of passing `measure_colnames`.
@@ -252,38 +235,31 @@ df_predictors_daily[predictors] = pred_scaler.transform(df_predictors_daily[pred
 # This gives us a TimeSeriesDataset with two tensors. For predictors, we need to impute missing values.
 
 # +
-dataset_pred = TimeSeriesDataset.from_dataframe(
-    dataframe=df_predictors_daily,
-    dt_unit='D',
+dataset_all = TimeSeriesDataset.from_dataframe(
+    dataframe=df_aq_weekly,
+    dt_unit='W',
     group_colname='station',
     time_colname='date',
-    y_colnames=['CO_log_scaled'],
-    X_colnames=predictors
+    y_colnames=measures_pp,
+    X_colnames=predictors_pp
 )
 
+dataset_train, dataset_val = dataset_all.train_val_split(dt=SPLIT_DT)
+
 # impute nans (since standardized, imputing w/zeros means imputing w/mean)
-_, X = dataset_pred.tensors
-X[torch.isnan(X)] = 0.0
-
-dataset_train, dataset_val = dataset_pred.train_val_split()
-dataset_train, dataset_val
-
-# since realigned, creates new nan-padding:
-_, X = dataset_train.tensors
-X[torch.isnan(X)] = 0.0
-_, X = dataset_val.tensors
-X[torch.isnan(X)] = 0.0
+for _dataset in (dataset_all, dataset_train, dataset_val):
+    _, X = _dataset.tensors
+    X[torch.isnan(X)] = 0.0
 # -
 
 # We add these predictors by adding a `LinearModel` process to our model. 
 
 # +
 kf_pred = KalmanFilter(
-    measures=['CO_log_scaled'],
-    processes=[
-        LocalLevel(id='local_level').add_measure('CO_log_scaled'),
-        FourierSeason(id='day_in_year', seasonal_period=365.25, dt_unit='D', K=2, fixed=True).add_measure('CO_log_scaled'),
-        LinearModel(id='predictors', covariates=predictors).add_measure('CO_log_scaled')
+    measures=measures_pp,
+    processes=processes + [
+        LinearModel(id=f'{m}_predictors', covariates=predictors_pp).add_measure(m) 
+        for m in measures_pp
     ]
 )
 
@@ -300,7 +276,7 @@ def closure():
 
 # -
 
-for epoch in range(10):
+for epoch in range(20):
     train_loss = kf_pred.opt.step(closure).item()
     y, X = dataset_val.tensors
     with torch.no_grad():
@@ -308,11 +284,9 @@ for epoch in range(10):
         val_loss = -pred.log_prob(y).mean().item()
     print(f"EPOCH {epoch}, TRAIN LOSS {train_loss}, VAL LOSS {val_loss}")
 
-# Plotting our predictions and components suggests that the predictors are doing a lot of the work previously done by `LocalLevel` and `FourierSeason`.
-
 # +
 y, _ = dataset_train.tensors
-_, X = dataset_pred.tensors
+_, X = dataset_all.tensors
 pred = kf_pred(
     y,
     predictors=X,
@@ -320,12 +294,14 @@ pred = kf_pred(
     out_timesteps=X.shape[1]
 )
 
-print(pred.plot(pred.to_dataframe(dataset_pred).query("group=='Aotizhongxin'"), split_dt=SPLIT_DT))
+print(
+    pred.plot(inverse_transform(pred.to_dataframe(dataset_all).query("group=='Changping'"), col_means),split_dt=SPLIT_DT)
+)
 
-df_components = pred.to_dataframe(dataset_pred, type='components')
+df_components = pred.to_dataframe(dataset_all, type='components')
 
-print(pred.plot(df_components.query("group=='Aotizhongxin'"), split_dt=SPLIT_DT))
+print(pred.plot(df_components.query("group=='Changping'"), split_dt=SPLIT_DT))
 # -
 # If we plot just the `predictors` process, then we can get a legend, helping us understand what each predictor contributes to the forecast:
 
-print(pred.plot(df_components.query("process == 'predictors'"), max_num_groups=2, split_dt=SPLIT_DT))
+print(pred.plot(df_components.query("(group=='Changping') & (process.str.endswith('predictors'))"), split_dt=SPLIT_DT))
