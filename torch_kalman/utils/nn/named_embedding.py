98f8fd93ce74884404916d9759133787d6a5118d
Jacob <jacobwdink@gmail.com>
2020-11-16 11:25:22 -0600

    Never add new group during eval

--- torch_kalman/utils/nn/named_embedding.py
+++ torch_kalman/utils/nn/named_embedding.py
@@ -1,4 +1,4 @@
-from typing import Optional, Sequence, Hashable, Dict
+from typing import Sequence, Dict
 from warnings import warn
 
 import torch
@@ -44,15 +44,14 @@ class NamedEmbedding(Embedding):
         for nm in input:
             nm = str(nm).rstrip()
             if nm not in self.name_to_idx:
+                if not self.training:
+                    indices.append(-1)
+                    continue
                 if len(self.name_to_idx) >= self.num_embeddings:
-                    if self.training:
-                        raise RuntimeError(
-                            f"Got a new group name '{nm}', but all {self.num_embeddings} idx are taken by previous "
-                            f"groups. If trying to predict for new groups set `self.train(False)`"
-                        )
-                    else:
-                        indices.append(-1)
-                        continue
+                    raise RuntimeError(
+                        f"Got a new group name '{nm}', but all {self.num_embeddings} idx are taken by previous "
+                        f"groups. If trying to predict for new groups set `self.train(False)`"
+                    )
                 else:
                     self._names_as_ints[len(self.name_to_idx)] = torch.tensor(_str_to_ints(nm, width=self.max_name_len))
                     self._name_to_idx = None  # reset `name_to_idx` cache
@@ -64,9 +63,10 @@ class NamedEmbedding(Embedding):
 
     def forward(self, input: Sequence[str]) -> torch.Tensor:
         idx = self._input_to_idx(input)
-        legal_idx = np.where(idx >= 0)[0]
         out = torch.zeros((len(input), self.embedding_dim))
-        out[legal_idx] = super().forward(idx[legal_idx])
+        if (idx >= 0).any():
+            legal_idx = np.where(idx >= 0)[0]
+            out[legal_idx] = super().forward(idx[legal_idx])
         return out
 
     def reset_parameters(self):
