ef8026b9bf61a6f982331eff583d1b05358fa41f
Jacob <jacob.dink@strong.io>
2021-04-05 16:32:44 -0500

    Simplify aq example

--- docs/examples/air_quality.py
+++ docs/examples/air_quality.py
@@ -52,11 +52,11 @@ from torchcast.process import LocalTrend, Season
 
 # create a dataset:
 df_aq['PM'] = df_aq['PM10'] + df_aq['PM2p5'] 
-df_aq['PMlog10'] = np.log10(df_aq['PM']) # TODO: underscore
+df_aq['PM_log10'] = np.log10(df_aq['PM']) 
 dataset_pm_univariate = TimeSeriesDataset.from_dataframe(
     dataframe=df_aq,
     dt_unit='W',
-    measure_colnames=['PMlog10'],
+    measure_colnames=['PM_log10'],
     group_colname='station', 
     time_colname='week'
 )
@@ -64,7 +64,7 @@ dataset_pm_univariate_train, _ = dataset_pm_univariate.train_val_split(dt=SPLIT_
 
 # create a model:
 kf_pm_univariate = KalmanFilter(
-    measures=['PMlog10'], 
+    measures=['PM_log10'], 
     processes=[
         LocalTrend(id='trend'),
         Season(id='day_in_year', period=365.25 / 7, dt_unit='W', K=5)
@@ -107,14 +107,20 @@ print(forecast.plot(df_forecast, max_num_groups=3, split_dt=SPLIT_DT))
 # #### Evaluating Performance: Expanding Window
 #
 #
-# TODO: explain
+# To evaluate our forecasts, we will not use the long-range forecasts above. Instead, we will use an [expanding window](https://eng.uber.com/forecasting-introduction/) approach to evaluate a shorter forecast horizon. In this approach, we generate N-step-ahead forecasts at every timepoint:
+#
+# ![title](expanding_window.png)
+#
+#
+# This approach is straightforward in `torchcast`, using the `n_step` argument. Here we'll generate 4-week-ahead predictions. Note that we're still separating the validation time-period.
 
 # +
-pred_4step = kf_pm_univariate(
-    dataset_pm_univariate.tensors[0],
-    start_datetimes=dataset_pm_univariate.start_datetimes,
-    n_step=4
-)
+with torch.no_grad():
+    pred_4step = kf_pm_univariate(
+        dataset_pm_univariate.tensors[0],
+        start_datetimes=dataset_pm_univariate.start_datetimes,
+        n_step=4
+    )
 
 df_univariate_error = pred_4step.\
     to_dataframe(dataset_pm_univariate, group_colname='station', time_colname='week').\
@@ -161,18 +167,35 @@ kf_pm_multivariate.fit(
     dataset_pm_multivariate_train.tensors[0],
     start_datetimes=dataset_pm_multivariate_train.start_datetimes
 )
+# -
 
-# +
+# We can generate our 4-step-ahead predictions for validation as we did before:
+
 with torch.no_grad():
     pred_4step = kf_pm_multivariate(
         dataset_pm_multivariate.tensors[0],
         start_datetimes=dataset_pm_multivariate.start_datetimes,
         n_step=4
     )
-    # generate draws from the forecast distribution:
-    mc_draws = 10 ** torch.distributions.MultivariateNormal(*pred_4step).rsample((500,))
-    # sum across 2.5 and 10, then mean across draws:
-    mc_predictions = mc_draws.sum(-1, keepdim=True).mean(0)
+pred_4step.means.shape
+
+# At this point, though, we run into a problem: we we have forecasts for both PM2.5 and PM10, but we ultimately want a forecast for their *sum*. With untransformed data, we could take advantage of the fact that [sum of correlated normals is still normal](https://en.wikipedia.org/wiki/Sum_of_normally_distributed_random_variables#Correlated_random_variables):
+
+torch.sum(pred_4step.means, 2)
+
+# In our case this unfortunately won't work: we have log-transformed our measures. This seems like it was the right choice (i.e. our residuals look reasonably normal and i.i.d):
+
+pred_4step.plot(pred_4step.to_dataframe(dataset_pm_multivariate, type='components').query("process=='residuals'"))
+
+# In this case, we **can't take the sum of our forecasts to get the forecast of the sum**, and [there's no simple closed-form expression for the sum of lognormals](https://scholar.google.com/scholar?hl=en&as_sdt=0%2C14&q=SUMS+OF+LOGNORMALS&btnG=).
+#
+# One option that is fairly easy in `torchcast` is to use a [Monte-Carlo](https://en.wikipedia.org/wiki/Monte_Carlo_method) approach: we'll just generate random-samples based on the means and covariances underlying our forecast. In that case, the sum of the PM2.5 + PM10 forecasted-samples *is* the forecasted PM sum we are looking for:
+
+# +
+# generate draws from the forecast distribution:
+mc_draws = 10 ** torch.distributions.MultivariateNormal(*pred_4step).rsample((500,))
+# sum across 2.5 and 10, then mean across draws:
+mc_predictions = mc_draws.sum(-1, keepdim=True).mean(0)
     
 # convert to a dataframe and summarize error:
 _df_pred = TimeSeriesDataset.tensor_to_dataframe(
@@ -195,221 +218,9 @@ df_multivariate_error = _df_pred.\
 df_multivariate_error.groupby('validation')['error'].agg(['mean','std'])
 # -
 
+# We see that this approach has reduced our error: substantially in the training period, and moderately in the validation period. We can look at the per-site differences to reduce common sources of noise and see that the reduction is consistent (it holds for all but one site):
+
 df_multivariate_error.\
     merge(df_univariate_error, on=['station', 'validation']).\
     assign(error_diff = lambda df: df['error_x'] - df['error_y']).\
     boxplot('error_diff', by='validation')
-
-# ### Incorporating Predictors 
-
-# +
-from torchcast.process import LinearModel
-
-# create lagged predictors:
-predictors = pd.Series(['TEMP','PRES', 'DEWP'])
-df_lagged_preds = df_aq.set_index('week').groupby('station')[predictors].shift(4, freq='7D')
-df_lagged_preds.columns = df_lagged_preds.columns + '_lag4'
-predictors = df_lagged_preds.columns
-df_lagged_preds -= df_lagged_preds.mean()
-df_lagged_preds /= df_lagged_preds.std()
-
-# create a dataset:
-dataset_pm_multivariate = TimeSeriesDataset.from_dataframe(
-    dataframe=df_aq.merge(df_lagged_preds.reset_index()),
-    dt_unit='W',
-    y_colnames=['PM10_log10','PM2p5_log10'],
-    X_colnames=predictors,
-    group_colname='station', 
-    time_colname='week'
-)
-dataset_pm_multivariate_train, _ = dataset_pm_multivariate.train_val_split(dt=SPLIT_DT)
-
-# create a model:
-_processes = []
-for m in dataset_pm_multivariate.measures[0]:
-    _processes.extend([
-        LocalTrend(id=f'{m}_trend', measure=m),
-        LinearModel(id=f'{m}_lm', measure=m, predictors=predictors),
-        Season(id=f'{m}_day_in_year', period=365.25 / 7, dt_unit='W', K=5, measure=m)
-    ])
-kf_pm_lm = KalmanFilter(measures=dataset_pm_multivariate.measures[0], processes=_processes)
-
-# fit:
-y, X = dataset_pm_multivariate_train.tensors
-kf_pm_lm.fit(
-    y,
-    X=X,
-    start_datetimes=dataset_pm_multivariate_train.start_datetimes
-)
-
-# +
-with torch.no_grad():
-    y, X = dataset_pm_multivariate.tensors
-    pred_4step = kf_pm_lm(
-        y,
-        X=X,
-        start_datetimes=dataset_pm_multivariate.start_datetimes,
-        n_step=4
-    )
-    mc_draws = 10 ** torch.distributions.MultivariateNormal(*pred_4step).rsample((500,))
-    mc_predictions = mc_draws.sum(-1, keepdim=True).mean(0)
-_df_pred = TimeSeriesDataset.tensor_to_dataframe(
-    mc_predictions, 
-    times=dataset_pm_multivariate.times(),
-    group_names=dataset_pm_multivariate.group_names,
-    group_colname='station',
-    time_colname='week',
-    measures=['predicted']
-)    
-
-df_mv_lm_error = _df_pred.\
-    merge(df_aq.loc[:,['station', 'week', 'PM']]).\
-    assign(
-        error = lambda df: np.abs(df['predicted'] - df['PM']),
-        validation = lambda df: df['week'] > SPLIT_DT
-    ).\
-    groupby(['station','validation'])\
-    ['error'].mean().\
-    reset_index()
-df_mv_lm_error.groupby('validation')['error'].agg(['mean','std'])
-# -
-
-df_mv_lm_error.\
-    merge(df_multivariate_error, on=['station', 'validation']).\
-    assign(error_diff = lambda df: df['error_x'] - df['error_y']).\
-    boxplot('error_diff', by='validation')
-
-# ### Predicting State with a Neural Network
-
-# +
-from torchcast.process import NN
-
-# XXX
-state_nn = torch.nn.Sequential(torch.nn.Linear(len(predictors), 10), torch.nn.Tanh(), torch.nn.Linear(10, 2))
-
-_processes = []
-for m in dataset_pm_multivariate.measures[0]:
-    _processes.extend([
-        LocalTrend(id=f'{m}_trend', measure=m),
-        NN(id=f'{m}_nn', measure=m, nn=state_nn),
-        Season(id=f'{m}_day_in_year', period=365.25 / 7, dt_unit='W', K=5, measure=m)
-    ])
-kf_pm_nn = KalmanFilter(measures=dataset_pm_multivariate.measures[0], processes=_processes)
-
-
-# -
-
-class EarlyStopping:
-    def __init__(self, 
-                 kf: KalmanFilter, 
-                 holdout_dataset: TimeSeriesDataset, 
-                 recent_len: int = 3, 
-                 tol: float = .001):
-        self.kf = kf
-        self.holdout_dataset = holdout_dataset
-        self.lh = []
-        assert recent_len > 1
-        self.recent_len = recent_len
-        self.tol = tol
-    
-    @torch.no_grad()
-    def __call__(self, *args):
-        y, X = self.holdout_dataset.tensors
-        pred = self.kf(y, X=X, start_datetimes=self.holdout_dataset.start_datetimes)
-        self.lh.append(-pred.log_prob(y).mean().item())
-        if len(self.lh) >= self.recent_len:
-            loss_decrease = -pd.Series(self.lh[-self.recent_len:]).diff()
-            if loss_decrease.max() < self.tol:
-                print(f"Holdout loss has not improved in last {self.recent_len-1}, stopping")
-                raise KeyboardInterrupt
-
-
-# +
-
-# fit:
-y, X = dataset_pm_multivariate_train[0:11].tensors
-kf_pm_nn.fit(
-    y,
-    X=X,
-    start_datetimes=dataset_pm_multivariate_train.start_datetimes[0:11],
-    callbacks=[EarlyStopping(kf=kf_pm_nn, holdout_dataset=dataset_pm_multivariate_train[11:])]
-)
-
-# +
-with torch.no_grad():
-    y, X = dataset_pm_multivariate.tensors
-    pred_4step = kf_pm_nn(
-        y,
-        X=X,
-        n_step=4,
-        start_datetimes=dataset_pm_multivariate.start_datetimes
-    )
-    mc_draws = 10 ** torch.distributions.MultivariateNormal(*pred_4step).rsample((500,))
-    mc_predictions = mc_draws.sum(-1, keepdim=True).mean(0)
-_df_pred = TimeSeriesDataset.tensor_to_dataframe(
-    mc_predictions, 
-    times=dataset_pm_multivariate.times(),
-    group_names=dataset_pm_multivariate.group_names,
-    group_colname='station',
-    time_colname='week',
-    measures=['predicted']
-)    
-
-df_mv_nn_error = _df_pred.\
-    merge(df_aq.loc[:,['station', 'week', 'PM']]).\
-    assign(
-        error = lambda df: np.abs(df['predicted'] - df['PM']),
-        validation = lambda df: df['week'] > SPLIT_DT
-    ).\
-    groupby(['station','validation'])\
-    ['error'].mean().\
-    reset_index()
-df_mv_nn_error.groupby('validation')['error'].agg(['mean','std'])
-# -
-
-df_mv_nn_error.\
-    merge(df_multivariate_error, on=['station', 'validation']).\
-    assign(error_diff = lambda df: df['error_x'] - df['error_y']).\
-    boxplot('error_diff', by='validation')
-
-)
-
-_processes = []
-    _processes.extend([
-        LocalTrend(id=f'{m}_trend', measure=m),
-    ])
-kf_pm_nn2 = KalmanFilter(
-    measures=dataset_pm_multivariate.measures[0], 
-    processes=_processes,
-    measure_covariance=Covariance.for_measures(
-        measures=dataset_pm_multivariate.measures[0], predict_variance=predict_mvar
-    ),
-    process_covariance=Covariance.for_processes(
-        processes=_processes, predict_variance=predict_pvar
-    )
-)
-
-# fit:
-    y, 
-    X=X, 
-)
-
-# +
-with torch.no_grad():
-        y,
-        X=X,
-    )
-    mc_draws = 10 ** torch.distributions.MultivariateNormal(*pred_4step).rsample((500,))
-    mc_predictions = mc_draws.sum(-1, keepdim=True).mean(0)
-_df_pred = TimeSeriesDataset.tensor_to_dataframe(
-    mc_predictions, 
-    group_colname='station',
-    time_colname='week',
-    measures=['predicted']
-)    
-
-    merge(df_aq.loc[:,['station', 'week', 'PM']]).\
-# -
-
-
-
